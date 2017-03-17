7eb28e2f390136877fcd6f182887406868bbbea0
Garrett Folks <folksgl@dukes.jmu.edu>
2017-03-17 18:29:01 -0400

    fixed copying error

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # install VimPlug
- ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
